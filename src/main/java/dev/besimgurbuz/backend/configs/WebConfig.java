67ba1d257bc738e7b1abb6736e77c291dfaa5698
besimgurbuz <mbesimgurbuz@gmail.com>
2022-02-14 05:30:59 +0300

    start taking allowed origin from env variable

--- src/main/java/dev/besimgurbuz/backend/configs/WebConfig.java
+++ src/main/java/dev/besimgurbuz/backend/configs/WebConfig.java
@@ -1,9 +1,13 @@
 package dev.besimgurbuz.backend.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Besim Gurbuz
@@ -11,8 +15,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    private static final String TAG = "WebConfig";
+    private static final Logger logger = Logger.getLogger(TAG);
+
+    @Value("${cors.allowed_origin}")
+    private String allowedOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://www.besimgurbuz.dev");
+        logger.log(Level.INFO, "Allowing all requests from origin: {0}", allowedOrigin);
+        registry.addMapping("/**").allowedOrigins(allowedOrigin);
     }
 }
