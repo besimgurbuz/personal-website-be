9af0e5c7ff039b47e75596c2f67d6e5d1f8f3b0e
besimgurbuz <mbesimgurbuz@gmail.com>
2022-02-19 16:01:20 +0300

    add profile to SpotifyRedirectController

--- src/main/java/dev/besimgurbuz/backend/recent/controllers/SpotifyRedirectController.java
+++ src/main/java/dev/besimgurbuz/backend/recent/controllers/SpotifyRedirectController.java
@@ -2,6 +2,7 @@ package dev.besimgurbuz.backend.recent.controllers;
 
 import dev.besimgurbuz.backend.recent.services.SpotifyAuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/spotify")
+@Profile("!prod")
 public class SpotifyRedirectController {
     private final SpotifyAuthenticationService spotifyAuthenticationService;
 
